19c50f2285fb1b178cdf45dd3406c09406f7fe94
codeminnam <code.minnam@gmail.com>
2020-10-11 02:50:46 +0900

    fix: Fix Edit page, container, and component

--- src/containers/EditContainer.tsx
+++ src/containers/EditContainer.tsx
@@ -1,38 +1,59 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import Edit from '../components/Edit';
 import { logout as logoutSaga } from '../redux/modules/auth';
 import { RootState } from '../redux/modules/rootReducer';
-import { getBooks, editBook as editBookSaga } from '../redux/modules/books';
-import { BookReqType } from '../types';
+import {
+  editBook as editBookSaga,
+  getBooks as getBooksSaga,
+} from '../redux/modules/books';
+import { useParams } from 'react-router-dom';
+import { goBack } from 'connected-react-router';
 
-interface EditContainerProps {
-  bookId: number
-  goBack: () => void;
-}
 
-const EditContainer: React.FC<EditContainerProps> = ({ bookId, goBack }) => {
-  const { books } = useSelector((state: RootState) => state.books);
+const EditContainer = () => {
+  const { id } = useParams();
+  const bookId = Number(id) || -1;
+
+  const { books, loading, error } = useSelector((state: RootState) => state.books);
+
   const dispatch = useDispatch();
+
+  const back = useCallback(() => {
+    dispatch(goBack());
+  }, [dispatch]);
+
   const logout = useCallback(() => {
     dispatch(logoutSaga());
   }, [dispatch]);
 
   // [project] saga 함수를 실행하는 액션 생성 함수를 실행하는 함수를 컨테이너에 작성했다.
-  useEffect(() => {
-    dispatch(getBooks())
+  const getBooks = useCallback(() => {
+    dispatch(getBooksSaga());
   }, [dispatch]);
 
-  const book = books?.find(book => book.bookId === bookId);
-  const editBook = ({ title, message, author, url }: BookReqType) => {
-    dispatch(editBookSaga(bookId, { title, message, author, url }));
-  }
+  const editBook = useCallback(
+    (book) => {
+      dispatch(editBookSaga(bookId, book));
+    },
+    [dispatch, bookId],
+  );
 
   // [project] 컨테이너에서 useDispatch, useSelector, useCallback 을 활용해서 중복없이 비동기 데이터를 보여주도록 처리했다.
   // [project] Edit 나 Detail 컴포넌트에서 새로고침 시, 리스트가 없는 경우, 리스트를 받아오도록 처리했다.
-
-  return <Edit book={book} loading={false} logout={logout} goBack={goBack} editBook={editBook} />;
+  return (
+    <Edit
+      book={
+        books === null ? null : books.find((book) => book.bookId === bookId)
+      }
+      loading={loading}
+      error={error}
+      getBooks={getBooks}
+      editBook={editBook}
+      goBack={back}
+      logout={logout} />
+  );
 };
 
 export default EditContainer;
--- src/pages/Edit.tsx
+++ src/pages/Edit.tsx
@@ -3,25 +3,14 @@ import { Redirect } from 'react-router-dom';
 
 import useToken from '../hooks/useToken';
 import EditContainer from '../containers/EditContainer';
-import { RouteComponentProps } from 'react-router';
 
-interface Params {
-  id: string;
-}
-
-const Edit: React.FC<RouteComponentProps<Params>> = ({ match, history }) => {
+const Edit = () => {
   const token = useToken();
   if (token === null) {
     return <Redirect to="/signin" />;
   }
 
-  const goBack = () => {
-    history.goBack();
-  }
-
-  const bookId = parseInt(match.params.id, 10);
-
-  return <EditContainer bookId={bookId} goBack={goBack} />;
+  return <EditContainer />;
 };
 
 export default Edit;
